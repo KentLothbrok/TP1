fc6f5136d692b27885c7dcc7ecf61b67ec03c6f5
ragnar <quentin.chazotte@live.fr>
2017-11-24 13:56:28 +0100

    Build APK

--- app/src/main/java/com/example/ragnar/tp1/MainActivity2.java
+++ app/src/main/java/com/example/ragnar/tp1/MainActivity2.java
@@ -24,5 +24,6 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
     public void onClick(View view) {
         Intent intent = new Intent(MainActivity2.this, MainActivity.class);
         startActivity(intent);
+        finish();
     }
 }
